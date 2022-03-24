e8069f9cb6bea0806128d022e7dbd53e9ac8eed3
mmohideen <mmohideen@vmware.com>
2022-03-24 21:03:39 +0530

    "Stream App"

--- src/main/java/com/pluralsight/flink/TweetsProcessor.java
+++ src/main/java/com/pluralsight/flink/TweetsProcessor.java
@@ -1,7 +1,6 @@
 package com.pluralsight.flink;
 
 import java.util.Properties;
-import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.twitter.TwitterSource;
 
